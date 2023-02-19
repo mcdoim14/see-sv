6f169d68b714c5937e2e4e9f2480ac1660d2e41e
Ian McDonnell <mcdoim14@gmail.com>
2023-02-19 14:30:01 -0600

    Testing things out...

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <LandingPage></LandingPage>
 </template>
 
@@ -21,6 +20,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
+
 </style>
--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,16 +1,23 @@
 <template>
     <div class="LandingPageCls">
         <h1>THIS IS A TEST APP</h1>
+        <button class="TestButtonCls" @click="test">CLICK ME!</button>
         <body>
             Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah
         </body>
-        <button @click="test">CLICK ME!</button>
     </div>
 </template>
 
 <style scoped>
 .LandingPageCls {
+    width: 100%;
+    height: 10000px;
     background-color: aquamarine;
+}
+
+.TestButtonCls {
+    background-color: cornflowerblue;
+    margin: 10px;
 }
 </style>
 
