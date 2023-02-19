97ac4fdce50f7525528ac3f1878b8fbbcf5951ae
Ian McDonnell <mcdoim14@gmail.com>
2023-02-19 13:55:35 -0600

    Added LandingPage

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="LandingPageCls">
+        <h1>THIS IS A TEST APP</h1>
+        <body>
+            Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah
+        </body>
+        <button @click="test">CLICK ME!</button>
+    </div>
+</template>
+
+<style scoped>
+.LandingPageCls {
+    background-color: aquamarine;
+}
+</style>
+
+<script>
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        test() {
+            alert("This is a test alert!");
+        }
+    }    
+}
+</script>
