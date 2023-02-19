ff3c83c42fdf34ad4d8775bde172bee08926aaa6
Ian McDonnell <mcdoim14@gmail.com>
2023-02-19 17:50:51 -0600

    Added DevExtreme components.

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -2,6 +2,16 @@
     <div class="LandingPageCls">
         <h1>THIS IS A TEST APP</h1>
         <button class="TestButtonCls" @click="test">CLICK ME!</button>
+        <DxButton
+            text="Click me too!"
+            @click="sayHelloWorld"
+        />
+        <DxChart
+            :data-source="data">
+            <DxArgumentAxis :tick-interval="10" />
+            <DxSeries type="bar" />
+            <DxLegend :visible="false" />
+        </DxChart>
         <body>
             Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah
         </body>
@@ -22,16 +32,39 @@
 </style>
 
 <script>
+import DxButton from 'devextreme-vue/button';
+import DxChart, { DxArgumentAxis, DxSeries, DxLegend } from 'devextreme-vue/chart';
+
+const data = [{
+    arg: 1990,
+    val: 5320816667
+}, {
+    arg: 2000,
+    val: 6127700428
+}, {
+    arg: 2010,
+    val: 6916183482
+}];
 
 export default {
+    components: {
+        DxButton,
+        DxChart,
+        DxArgumentAxis,
+        DxSeries,
+        DxLegend
+    },
     data() {
         return {
-
+            data,
         }
     },
     methods: {
         test() {
             alert("This is a test alert!");
+        },
+        sayHelloWorld() {
+            alert('Hello world!');
         }
     }    
 }
